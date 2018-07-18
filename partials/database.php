07c0135650bb46d0281e88504054e580395bdcd7
Dragoness390 <cera11@hotmail.se>
2018-07-18 15:46:15 +0200

    try to make it easier to see the error instead of 500 error on the page

--- partials/database.php
+++ partials/database.php
@@ -1,7 +1,15 @@
 <?php $pdo = new PDO
     (
         "mysql:host=localhost;dbname=todo;charset=utf8",
-            "root",
-            "root"
+            "root",  // user
+            "root"   //password
     );
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ?>
+
+
+<!-- dbname means that I add the name of my database here which is todo -->
